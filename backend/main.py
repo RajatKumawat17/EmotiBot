55c23219bf37ecf3bfba6a8f9c6ed37cccc03c95
RajatKumawat17 <kumawatrajat124@gmail.com>
2025-01-15 17:58:42 +0530

    added frontend url

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://emoti-bot.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
